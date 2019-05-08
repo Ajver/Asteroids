f74c47ef17cb0a2ffc47535edd1b3e297cdd6179
Ajver <zawlockidominik@gmail.com>
2019-05-08 23:20:14 +0200

    Create basic main menu management

---
+++ Assets/Scripts/ButtonControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+
+    public Image lightImg;
+    
+    public void EnableLight()
+    {
+        setLight(lightImg, true);
+    }
+
+    public void DisableLight()
+    {
+        setLight(lightImg, false);
+    }
+
+    void setLight(Image img, bool value)
+    {
+        Light light = img.GetComponent<Light>();
+        light.turn(value);
+    }
+
+}
---
+++ Assets/Scripts/StartMenuControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartMenuControl : MonoBehaviour
+{
+
+    public Button startGameBtn;
+    public Button optionsBtn;
+    public Button quitBtn;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void EnterOptionsScene()
+    {
+        Debug.Log("Options!");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
