67fa251ed9f57bc6462bfea8c736951fdffe85b8
Ajver <zawlockidominik@gmail.com>
2019-05-09 12:05:24 +0200

    Create Transition between scenes

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private string sceneToLoadName;
+    
+    public void FadeToScene(string sceneName)
+    {
+        sceneToLoadName = sceneName;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComlete()
+    {
+        SceneManager.LoadScene(sceneToLoadName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
