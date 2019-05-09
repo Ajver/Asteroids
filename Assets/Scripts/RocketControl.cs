83b12d33bfd8f85bb5edbaa85d24a138d26b5069
Ajver <zawlockidominik@gmail.com>
2019-05-09 22:15:19 +0200

    Create rockets

---
+++ Assets/Scripts/RocketControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketControl : MonoBehaviour
+{
+
+    public float waitTime = 1.5f;
+
+    public GameObject sprite;
+
+    private bool isAlive = true;
+    
+    void Update()
+    {
+        if (!isAlive)
+            return;
+
+        waitTime -= Time.deltaTime;
+        
+        if(waitTime <= 0.0f)
+        {
+            Boom();
+        }
+    }
+
+    void Boom()
+    {
+        isAlive = false;
+        sprite.SetActive(false);
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        Destroy(gameObject, 0.5f);
+        Debug.Log("BOOM!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Asteroid")
+        {
+            Boom();
+        }
+    }
+
+}
---
+++ Assets/Scripts/RocketLauncher.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour
+{
+
+    public Transform firePoint;
+
+    public GameObject rocketPrefab;
+    public float rocketSpeed = 2.0f;
+    public float nextRocketWaitTime;
+
+    private float nextRocketTimer;
+
+    private bool isShipAlive = true;
+
+    void Start()
+    {
+        ShipControll.OnGameOver += OnGameOver;
+        RestartNextRocketTimer();
+    }
+
+    void Update()
+    {
+        if (!isShipAlive)
+            return;
+
+        nextRocketTimer -= Time.deltaTime;
+
+        if(nextRocketTimer <= 0.0f)
+        {
+            if(GetRocketShotButtonDown())
+            {
+                LaunchRocket();
+                RestartNextRocketTimer();
+            }
+        }
+
+    }
+
+    private bool GetRocketShotButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift);
+    }
+
+    void LaunchRocket()
+    {
+        RestartNextRocketTimer();
+        GameObject rocket = Instantiate(rocketPrefab, firePoint.position, firePoint.rotation);
+        rocket.GetComponent<Rigidbody2D>().velocity = firePoint.up * rocketSpeed;
+    }
+
+    private void RestartNextRocketTimer()
+    {
+        nextRocketTimer = nextRocketWaitTime;
+    }
+
+    public void OnGameOver()
+    {
+        isShipAlive = false;
+    }
+
+}
